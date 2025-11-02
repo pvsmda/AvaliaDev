cc22aad49471ba5eb8b3397550068d70d10a9f40
Paulo Victor <spaulovictor813@gmail.com>
2025-11-02 14:10:09 -0300

    auth sync db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { serve } from "inngest/express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import path from "path";
+import { inngest } from "./lib/inngest.js";
 
 dotenv.config();
 
 const app = express();
 
 const __dirname = path.resolve();
+
+// middleware
+app.use(express.json());
+
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({
